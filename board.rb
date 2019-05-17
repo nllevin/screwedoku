38edb1c17da0ecd56851288e6e4b3eb9a0da8641
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-16 20:57:37 -0400

    Fix mixup between grid and rows

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require 'byebug'
 
 class Board
   attr_reader :grid
@@ -35,7 +36,7 @@ class Board
   end
 
   def columns
-    rows.transpose
+    grid.transpose
   end
 
   def render
@@ -50,10 +51,8 @@ class Board
     grid.size
   end
 
-  alias_method :rows, :size
-
   def solved?
-    rows.all? { |row| solved_set?(row) } &&
+    grid.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
   end
