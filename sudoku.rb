fc6367c2edca8aa38fb4d97f8f5cd220028bdcd6
Noah Levin <50681582+nllevin@users.noreply.github.com>
2019-05-16 21:08:39 -0400

    Fix parse_pos in sudoku.rb

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,6 @@
 require_relative "board"
 require 'colorize'
+require "byebug"
 
 puts "Only contractors write code this bad.".yellow
 
@@ -13,13 +14,12 @@ class SudokuGame
     @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
+  # def method_missing(method_name, *args)
+  #   if method_name =~ /val/
+  #     Integer(1)
+
+  def parse_pos(string)
+      string.split(",").map! { |char| Integer(char) }
   end
 
   def get_pos
